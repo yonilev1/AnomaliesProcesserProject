e5bc5d5ff845776e31a9c2e149536b6c41c9d6bd
Yoni <yonilev2021@gmail.com>
2026-09-22 16:36:18 +0300

    started rew consumer - Consumers builder function

--- RewConsumer/Consumer/DataConsumer.cs
+++ RewConsumer/Consumer/DataConsumer.cs
@@ -12,7 +12,7 @@ public class DataConsumer : BackgroundService
     private readonly IServiceScopeFactory _factory;
     private readonly string _bootstrapServer;
     private readonly IConsumer<Null, string> _consumer;
-    private record string _topic;
+    private readonly string _topic;
 
     public DataConsumer(
         ILogger<DataConsumer> logger,
@@ -23,6 +23,13 @@ public class DataConsumer : BackgroundService
         _logger = logger;
         _factory = factory;
         _bootstrapServer = configuration["Kafka:BootstrapServers"] ?? "localhost:9092";
-        _topic = configuration["Kafka:Topics:StationsInformationTopic"] ?? "bike.station-information";
+        _topic = configuration["Kafka:Topic"] ?? "activity-readings";
+
+        ConsumerConfig config = new ConsumerConfig
+        {
+            BootstrapServers = _bootstrapServer,
+            GroupId = "activity-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
     }
 }
